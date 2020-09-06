f01e87c967d13f1522663bdf849712a2a271d181
Yelkrab-Studios <YelkrabStudios@gmail.com>
2020-09-06 14:50:17 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShadowBolt
+
+Developed with Unreal Engine 4
---
+++ Source/ShadowBolt/ShadowBoltGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShadowBoltGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWBOLT_API AShadowBoltGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
